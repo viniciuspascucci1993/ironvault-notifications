b9cc8815043a547ecb72d93d2e49dfa248201192
viniciuspascucci1993 <vinicius.pascucci1@gmail.com>
2026-05-09 15:44:10 -0300

    Project Created

---
+++ src/main/java/com/ironvault/notifications/IronvaultNotificationsApplication.java
@@ -0,0 +1,13 @@
+package com.ironvault.notifications;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronvaultNotificationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronvaultNotificationsApplication.class, args);
+	}
+
+}
